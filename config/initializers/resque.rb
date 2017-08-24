dd3b07e39b74693161a704e214c9b0ccb64200d1
Benjamin Jackson <bhjackson@gmail.com>
2017-08-24 18:26:55 -0400

    Switched namespace of Resque to a more generic one since it's shared with the crawler.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
 require 'resque'
 
 Resque.redis = Redis.new(host:ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], password: ENV['REDIS_PASS'])
-Resque.redis.namespace = "resque:scraper"
+Resque.redis.namespace = "resque"
