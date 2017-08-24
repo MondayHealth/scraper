c942d6226a9033cd5ab6135d671ef71a60a7a0db
Benjamin Jackson <bhjackson@gmail.com>
2017-08-24 18:27:31 -0400

    Fixed logging in background jobs (was missing newlines).

--- app/jobs/concerns/logged_job.rb
+++ app/jobs/concerns/logged_job.rb
@@ -2,15 +2,15 @@ module Jobs
   module Concerns
     module LoggedJob
       def before_perform_log_job(*args)
-        STDOUT.write "Performing #{self} with #{args.inspect}"
+        STDOUT.puts "Performing #{self} with #{args.inspect}"
       end
 
       def after_perform_log_job(*args)
-        STDOUT.write "Finished performing #{self} with #{args.inspect}"
+        STDOUT.puts "Finished performing #{self} with #{args.inspect}"
       end
 
       def on_failure_log_job(*args)
-        STDOUT.write "Failed #{self} with #{args.inspect}"
+        STDOUT.puts "Failed #{self} with #{args.inspect}"
       end
     end
   end
