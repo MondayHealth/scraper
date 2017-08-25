1e30b2ea2c3c1a215e3f085be1e95d3e3da52eb9
Benjamin Jackson <bhjackson@gmail.com>
2017-08-25 12:46:36 -0400

    Fixed resque initializer to work when no Redis password is set.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,6 @@
 require 'resque'
 
-Resque.redis = Redis.new(host:ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], password: ENV['REDIS_PASS'])
+options = { host:ENV['REDIS_HOST'], port: ENV['REDIS_PORT'] }
+options[:password] = ENV['REDIS_PASS'] unless ENV['REDIS_PASS'].to_s.empty?
+Resque.redis = Redis.new(options)
 Resque.redis.namespace = "resque"
